c9fd85272218cb4e6b78516ac26b068424a66ba3
“jaicsdx” <jaicsdx@gmail.com>
2023-03-17 12:44:37 -0400

    new changes to the button value

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-let board = document.querySelector(".board")
-board.style.gridTemplateColumns = "repeat(16, 1fr)" 
-board.style.gridTemplateRows = "repeat(16, 1fr)"
+let a = document.querySelector("[name='a']")
+let b = document.querySelector("[name='b']");
+document.querySelector("button").addEventListener("click", ()=>{
+    
+    pixalate(a)})
+
+
+function pixalate(a)
+{
+    let board = document.querySelector(".board")
+  
+board.style.gridTemplateColumns = `repeat(${a}, 1fr)`
+board.style.gridTemplateRows = `repeat(${a}, 1fr)`
 
 
 for (let i=0; i<256; i++)
@@ -9,7 +19,20 @@ for (let i=0; i<256; i++)
     square.style.backgroundColor ="red";
     board.insertAdjacentElement("beforeend",square)
     square.addEventListener("mouseover", ()=>{
-        square.style.backgroundColor ="blue";
+    square.style.backgroundColor ="blue";})
 
-    })
+
 }
+
+}
+
+
+
+
+    /* let a = document.querySelector("[name='a']"),
+      b = document.querySelector("[name='b']");
+
+      document.querySelector("button").addEventListener("click", ()=>{
+        const sum = Number(a.value)+Number(b.value);
+        alert(sum);
+    */
